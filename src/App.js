b61829221a9a4cfb6c1f53c5662265e99e8304ff
Pavel Chekalin <grokki91@gmail.com>
2024-10-15 18:26:01 +0300

    Add menu, spinner, persmission for admin

--- src/App.js
+++ src/App.js
@@ -1,53 +1,112 @@
-import { useState } from "react";
-import Auth from "./components/Auth";
-import Films from "./components/Films";
+import { useEffect, useState } from "react";
+import Auth from "./app/components/Auth";
+import HomePage from "./app/components/HomePage";
+import Spinner from "./app/components/Spinner";
 
 function App() {
-  const url = "http://localhost:8080/login";
+  useEffect(() => {
+    checkLogin();
+  }, []);
 
-  const [token, setToken] = useState('');
+  const [, setToken] = useState("");
   const [isLogin, setLogin] = useState(false);
+  const [message, setMessage] = useState("");
+  const [isSignUp, setSignUp] = useState(false);
+  const [isLoading, setLoading] = useState(false);
   const [data, setData] = useState({
-    username: '',
-    email: '',
-    password: '',
-    role: ''
+    username: "",
+    password: "",
+    email: "",
   });
 
-  const handleSubmit = e => {
-    const {name, value} = e.target
-    setData(prev => ({...prev, [name]: value}))
-  }
+  const checkFields = (fields) => {
+    return fields.some((field) => {
+      const value = data[field];
+      return value === null || value === "" || value === "undefined";
+    });
+  };
 
-  const fetchLoginRequest = async () => {
+  const loginFields = ["username", "password"];
+  const registerFields = ["username", "email", "password"];
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const fetchAuthRequest = async (url, fields) => {
+    const isObjectEmpty = checkFields(fields);
+
+    if (isObjectEmpty) {
+      setMessage("There are empty fields");
+      return;
+    }
+
     const options = {
-      method: "post",
+      method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
     };
 
-    await fetch(url, options)
-      .then((response) => {
-        return setToken(response.json());
-      })
-      .catch((e) => console.log(e));
+    try {
+      setLoading(true);
+      const response = await fetch(url, options);
+      const result = await response.json();
+
+      if (response.ok) {
+        setToken(result.token);
+        localStorage.setItem("token", result.token);
+        setLogin(true);
+      } else {
+        setMessage(result.Message);
+      }
+    } catch (error) {
+      setMessage("Server connection refused");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const toggleAuth = () => {
+    setSignUp((prev) => !prev);
+    setData({
+      username: "",
+      email: "",
+      password: "",
+    });
+    setMessage("");
   };
 
   let checkLogin = () => {
-    let tokenStorage = localStorage.getItem('token')
+    let tokenStorage = localStorage.getItem("token");
 
     if (tokenStorage) {
-      setLogin(true)
+      setToken(tokenStorage);
+      setLogin(true);
     }
-  }
+  };
 
-  return(
-    <>
-      {checkLogin ?
-      <Auth loginRequest={fetchLoginRequest} isLogin={isLogin} value={{...data}} sumbit={handleSubmit}/> :
-      <Films token={token} />}
-    </>
-  )
+  return (
+    <div className="container">
+      {isLoading && <Spinner />}
+      {isLogin ? (
+        <HomePage setLogin={setLogin} loading={isLoading} />
+      ) : (
+        <div className="auth-wrapper">
+          <Auth
+            authRequest={fetchAuthRequest}
+            isLogin={isLogin}
+            value={data}
+            onChange={handleChange}
+            message={message}
+            toggleAuth={toggleAuth}
+            isSignUp={isSignUp}
+            requireFields={isSignUp ? registerFields : loginFields}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
