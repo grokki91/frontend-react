a7b597d973590989444ea01ac51c3b25210a76ae
Pavel Chekalin <grokki91@gmail.com>
2024-12-06 19:10:26 +0300

    Add file media and fixed styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
+import './style/index.css';
+import './style/media.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
